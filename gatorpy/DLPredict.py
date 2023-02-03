edd41f21b12ca3585d836898cd290af7479fdd5e
Yapp <CLARENCE@HMS.HARVARD.EDU>
2023-02-03 15:09:36 -0500

    added new functions for training and predicting new gator UNet models

---
+++ gatorpy/DLPredict.py
@@ -0,0 +1,210 @@
+
+# Libs
+import os, argparse
+import pandas as pd
+import pathlib
+
+from UNet import *
+
+
+# Function
+def gatorPredict(imagePath, 
+                 outputDir, 
+                 markerChannelMapPath, 
+                 markerColumnName='marker', 
+                 channelColumnName='channel', 
+                 modelColumnName='gatormodel', 
+                 GPU = 0):
+    
+    """
+     --imagePath (str): The path to the .tif file that needs to be processed. This argument is required.
+    
+     --outputDir (str): The path to the output directory where the processed images will be saved.
+     
+     --markerChannelPath (str, optional): The path to the marker panel list, which contains information about the markers used in the image. This argument is required.
+     
+     --markerColumnName (str, optional): The name of the column in the marker panel list that contains the marker names. The default value is 'marker'.
+     
+     --channelColumnName (str, optional): The name of the column in the marker panel list that contains the channel names. The default value is 'channel'.
+     
+     --modelColumnName (str, optional): The name of the column in the marker panel list that contains the model names. The default value is 'gatormodel'.
+         
+     --scalingFactor (int, optional): A factor by which the image size will be increased/decreased. The default value is 1, meaning no change in size.
+     
+     --GPU (int, optional): An optional argument to explicitly select the GPU to use. The default value is -1, meaning that the GPU will be selected automatically.
+     
+     """
+    
+    fileName =pathlib.Path(imagePath).stem
+
+    # read the markers.csv
+    maper = pd.read_csv(pathlib.Path(markerChannelMapPath))
+    columnnames =  [word.lower() for word in maper.columns]
+    maper.columns = columnnames
+
+    # identify the marker column name (doing this to make it easier for people who confuse between marker and markers)
+    if markerColumnName not in columnnames:
+           if markerColumnName != 'marker':
+               raise ValueError('markerColumnName not found in markerChannelMap, please check')
+           if 'markers' in columnnames:
+               markerCol = 'markers'
+           else:
+               raise ValueError('markerColumnName not found in markerChannelMap, please check')
+    else:
+           markerCol = markerColumnName
+   
+
+   # identify the channel column name (doing this to make it easier for people who confuse between channel and channels)
+    if channelColumnName not in columnnames:
+        if channelColumnName != 'channel':
+            raise ValueError('channelColumnName not found in markerChannelMap, please check')
+        if 'channels' in columnnames:
+            channelCol = 'channels'
+        else:
+            raise ValueError('channelColumnName not found in markerChannelMap, please check')
+    else:
+        channelCol = channelColumnName
+
+
+    # identify the gator model column name (doing this to make it easier for people who confuse between gatormodel and gatormodels)
+    if modelColumnName not in columnnames:
+        if modelColumnName != 'gatormodel':
+            raise ValueError('modelColumnName not found in markerChannelMap, please check')
+        if 'gatormodels' in columnnames:
+            channelCol = 'gatormodels'
+        else:
+            raise ValueError('modelColumnName not found in markerChannelMap, please check')
+    else:
+        modelCol = modelColumnName
+
+    # remove rowa that have nans in modelCol
+    runMenu = maper.dropna(subset=[modelCol], inplace=False)[[channelCol,markerCol,modelCol]]
+
+    # shortcuts
+    numMarkers = len(runMenu)
+    len(runMenu.columns)
+
+    I = skio.imread(imagePath, img_num=0, plugin='tifffile')
+
+
+    probPath = pathlib.Path(outputDir + '/GATOR/gatorPredict/')
+    modelPath = pathlib.Path(outputDir + '/GATOR/gatorModels/')
+
+    if not os.path.exists(probPath):
+        os.makedirs(probPath,exist_ok=True)
+
+
+
+
+    def data(runMenu, 
+             imagePath, 
+             modelPath, 
+             outputDir, 
+             dsFactor=1, 
+             GPU=0):
+        
+        # Loop through the rows of the DataFrame
+        for index, row in runMenu.iterrows():
+            channel = row[channelColumnName]
+            markerName = row[markerColumnName]
+            gatormodel = row[modelColumnName]
+            print('Running gator model ' + str(gatormodel) + ' on channel ' + str(channel) + ' corresponding to marker ' + str(markerName) )
+
+
+            tf.reset_default_graph()
+            UNet2D.singleImageInferenceSetup(pathlib.Path(modelPath / gatormodel), GPU, -1, -1)
+
+            fileName = os.path.basename(imagePath)
+            fileNamePrefix = fileName.split(os.extsep, 1)
+            fileType = fileNamePrefix[1]
+            if fileType == 'ome.tif' or fileType == 'ome.tiff' or fileType == 'btf':
+                I = skio.imread(imagePath, img_num=int(channel-1), plugin='tifffile')
+            elif fileType == 'tif':
+                I = tifffile.imread(imagePath, key=int(channel-1))
+
+            if I.dtype == 'float32':
+                I=np.uint16(I)
+            rawVert = I.shape[0]
+            rawHorz = I.shape[1]
+            rawI = I
+
+            hsize = int(float(rawVert * float(dsFactor)))
+            vsize = int(float(rawHorz * float(dsFactor)))
+            # I = resize(I, (hsize, vsize))
+            cells = I
+            maxLimit = np.max(I)
+            I=I/65535*255
+
+            rawI = im2double(rawI) / np.max(im2double(rawI))
+
+            append_kwargs = {
+                'bigtiff': True,
+                'metadata': None,
+                'append': True,
+            }
+            save_kwargs = {
+                'bigtiff': True,
+                'metadata': None,
+                'append': False,
+            }
+
+            PM = np.uint8(255 * UNet2D.singleImageInference(I, 'accumulate',1))
+            PM = resize(PM, (rawVert, rawHorz))
+            yield np.uint8(255 * PM)
+
+    with tifffile.TiffWriter(probPath / (fileName + '_GatorOutput.ome.tif')) as tiff:
+        tiff.write(data(runMenu, imagePath, modelPath, probPath, dsFactor=1, GPU=0), shape=(numMarkers,I.shape[0],I.shape[1]), dtype='uint8', metadata={'Channel': {'Name': runMenu.marker.tolist()}, 'axes': 'CYX'})
+
+        UNet2D.singleImageInferenceCleanup()
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--imagePath", help="path to the .tif file")
+    parser.add_argument("--markerChannelPath", help="path to the marker panel list")
+    parser.add_argument("--markerColumnName", type=str, help="marker colulmn name in the marker panel list", default = 'marker')
+    parser.add_argument("--channelColumnName", type=str, help="channel column name in the marker panel list", default = 'channel')
+    parser.add_argument("--modelColumnName", type=str, help="model column name in the marker panel list", default = 'gatormodel')
+    parser.add_argument('--outputDir', type=str, help='Path to output directory.')
+    parser.add_argument("--scalingFactor", help="factor by which to increase/decrease image size by", type=float,
+                        default=1)
+    parser.add_argument("--GPU", help="explicitly select GPU", type=int, default=-1)
+    args = parser.parse_args()
+
+
+
+    logPath = ''
+    scriptPath = os.path.dirname(os.path.realpath(__file__))
+
+    pmPath = ''
+
+    if os.system('nvidia-smi') == 0:
+        if args.GPU == -1:
+            print("automatically choosing GPU")
+            GPU = GPUselect.pick_gpu_lowest_memory()
+        else:
+            GPU = args.GPU
+        print('Using GPU ' + str(GPU))
+
+    else:
+        if sys.platform == 'win32':  # only 1 gpu on windows
+            if args.GPU == -1:
+                GPU = 0
+                print('using default GPU')
+            else:
+                GPU = args.GPU
+            print('Using GPU ' + str(GPU))
+        else:
+            GPU = 0
+            print('Using CPU')
+    os.environ['CUDA_VISIBLE_DEVICES'] = '%d' % GPU
+
+
+    gatorPredict(imagePath = args.imagePath,
+              markerChannelMapPath = args.markerChannelPath,
+              outputDir = args.outputDir,
+              markerColumnName = 'marker',
+              channelColumnName = 'channel',
+              modelColumnName = 'gatormodel',
+              GPU = GPU)
---
+++ gatorpy/DLTrain.py
@@ -0,0 +1,208 @@
+
+
+# Libs
+import os, argparse, pathlib
+from UNet import *
+
+# Function
+def gatorTrain (TrainingDataPath, 
+			 outputDir,
+			 artefactPath=None, 
+			 modelName=None, 
+			 imSize=64,
+			 nChannels=1,
+			 nClasses=2,
+			 nExtraConvs=0,
+			 nLayers=3,
+			 featMapsFact=2,
+			 downSampFact=2,
+			 ks=3,
+			 nOut0=16,
+			 stdDev0=0.03,
+			 batchSize=16,
+			 epochs=100):
+	"""
+    
+Parameters:
+
+	TrainingDataPath : TYPE
+		The file path leading to the directory that holds the training data for each marker.
+		
+	artefactPath (str): 
+		Path to the directory where the artefacts data is loaded from.
+
+	outputDir (str): 
+		PatPath to output directory.
+	
+   modelName (str, optional): 
+	   Name of the model to be used. If None, the folder name will be used.
+   
+   imSize (int, optional): 
+	   Image size (assumed to be square).
+   
+   nChannels (int, optional): 
+	   Number of channels in the input image.
+   
+   nClasses (int, optional): 
+	   Number of classes in the classification problem. 
+   
+   nExtraConvs (int, optional): 
+	   Number of extra convolutional layers to add to the model. 
+   
+   nLayers (int, optional): 
+	   Total number of layers in the model. 
+   
+   featMapsFact (int, optional): 
+	   Factor to multiply the number of feature maps by in each layer.
+   
+   downSampFact (int, optional): 
+	   Factor to down-sample the feature maps by in each layer.
+   
+   ks (int, optional): 
+	   Kernel size for the convolutional layers.
+   
+   nOut0 (int, optional): 
+	   Number of filters in the first layer.
+   
+   stdDev0 (float, optional): 
+	   Standard deviation for the initializer for the first layer.
+   
+   batchSize (int, optional): 
+	   Batch size for training.
+   
+   epochs (int, optional): 
+	   Number of training epochs.
+
+Returns:
+
+	Deep learning model (images and model).
+
+Example:
+
+	```python
+	
+	
+	
+	```
+
+	"""
+
+	# convert to path
+	TrainingDataPath = pathlib.Path(TrainingDataPath)
+	
+	
+	# resolve model name
+	if modelName is None:
+		finalName = TrainingDataPath.stem
+	else:
+		finalName = modelName
+		
+	# optional artifacts
+	if artefactPath is not None:
+		artefactPath = pathlib.Path(artefactPath)
+		artefactTrainPath = pathlib.Path(artefactPath / 'training')
+		artefactValidPath = pathlib.Path(artefactPath / 'validation')
+	else:
+		artefactPath=''; artefactTrainPath=''; artefactValidPath=''
+
+	# paths for loading data
+	trainPath = pathlib.Path(TrainingDataPath / 'training')
+	validPath = pathlib.Path(TrainingDataPath / 'validation')
+	testPath = pathlib.Path(TrainingDataPath / 'test')
+
+
+	# Paths for saving data
+	logPath = pathlib.Path(outputDir + '/GATOR/DLTrain/tempTFLogs_' + finalName)
+	modelPath = pathlib.Path(outputDir + '/GATOR/DLmodels' + finalName)
+	pmPath = pathlib.Path(outputDir + '/GATOR/DLTrain/TFprobMaps' + finalName)
+	
+	# set up the model
+	UNet2D.setup(   imSize=imSize,
+					nClasses=nClasses,
+					nChannels = nChannels,
+					nExtraConvs=nExtraConvs,
+					nDownSampLayers=nLayers,
+					featMapsFact=featMapsFact,
+					downSampFact=downSampFact,
+					kernelSize=ks,
+					nOut0=nOut0,
+					stdDev0=stdDev0,
+					batchSize=batchSize)  # 4 (Ki67 CD8a) , 3 (CD45 ecad CD4 CD3D)
+		
+	
+	# train the model
+	UNet2D.train(trainPath=trainPath, 
+				  validPath=validPath, 
+				  testPath=testPath, 
+				  artTrainPath=artefactTrainPath, 
+				  artValidPath=artefactValidPath,
+				  logPath=logPath, 
+				  modelPath=modelPath, 
+				  pmPath=pmPath, 
+				  restoreVariables=False, 
+				  nSteps=epochs, 
+				  gpuIndex=0, 
+				  testPMIndex=2)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Train a deep learning model using data for each marker, stored in separate directories.')
+	parser.add_argument('--TrainingDataPath', type=str, help='The file path leading to the directory that holds the training data for each marker.')
+	parser.add_argument('--outputDir', type=str, help='Path to output directory.')
+	parser.add_argument('--artefactPath', type=str, help='Path to the directory where the artefacts data is loaded from.', default=None)
+	parser.add_argument('--modelName', type=str, help='Name of the model to be used. If None, the folder name will be used.', default=None)
+	parser.add_argument('--imSize', type=int, help='Image size (assumed to be square).', default=64)
+	parser.add_argument('--nChannels', type=int, help='Number of channels in the input image.', default=1)
+	parser.add_argument('--nClasses', type=int, help='Number of classes in the classification problem.', default=2)
+	parser.add_argument('--nExtraConvs', type=int, help='Number of extra convolutional layers to add to the model.', default=0)
+	parser.add_argument('--nLayers', type=int, help='Total number of layers in the model.', default=3)
+	parser.add_argument('--featMapsFact', type=int, help='Factor to multiply the number of feature maps by in each layer.', default=2)
+	parser.add_argument('--downSampFact', type=int, help='Factor to down-sample the feature maps by in each layer.', default=2)
+	parser.add_argument('--ks', type=int, help='Kernel size for the convolutional layers.', default=3)
+	parser.add_argument('--nOut0', type=int, help='Number of filters in the first layer.', default=16)
+	parser.add_argument('--stdDev0', type=float, help='Standard deviation for the initializer for the first layer.', default=0.03)
+	parser.add_argument('--batchSize', type=int, help='Batch size for training.', default=16)
+	parser.add_argument('--epochs', type=int, help='Number of training epochs.', default=100)
+	args = parser.parse_args()
+	DLTrain(TrainingDataPath=args.TrainingDataPath,
+			artefactPath=args.artefactPath,
+			outputDir=args.outputDir,
+			modelName=args.modelName,
+			imSize=args.imSize,
+			nChannels=args.nChannels,
+			nClasses=args.nClasses,
+			nExtraConvs=args.nExtraConvs,
+			nLayers=args.nLayers,
+			featMapsFact=args.featMapsFact,
+			downSampFact=args.downSampFact,
+			ks=args.ks,
+			nOut0=args.nOut0,
+			stdDev0=args.stdDev0,
+			batchSize=args.batchSize,
+			epochs=args.epochs)
+			
+
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+
